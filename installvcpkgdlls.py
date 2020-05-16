c6f38078ff9e4ef9a6de28e8b085faa43c436a8f
Trent Weiss <ttw2xk@virginia.edu>
2020-05-16 10:08:01 -0400

    yepp

--- installvcpkgdlls.py
+++ installvcpkgdlls.py
@@ -1,2 +1,20 @@
-import glob
-files = glob.glob(my_path + '/**/*.txt', recursive=True)
+import glob, os, argparse
+from shutil import copyfile
+parser = argparse.ArgumentParser("Automatically copy dll dependencies from vcpkg")
+parser.add_argument("buildpath", type=str)
+parser.add_argument("installbin", type=str)
+parser.add_argument("vcpkgbin", type=str)
+args = parser.parse_args()
+buildpath = args.buildpath
+installbin = args.installbin
+vcpkgbin = args.vcpkgbin
+files = glob.glob(os.path.join(buildpath,"**","*.dll"),recursive=True)
+for f in files:
+    basefile = os.path.basename(f)
+    installfile = os.path.join(installbin,basefile)
+    vcpkgfile = os.path.join(vcpkgbin,basefile)
+    if (not os.path.exists(installfile)) and os.path.exists(vcpkgfile):
+        print("Installing " + vcpkgfile + " to " + installfile)
+        copyfile(vcpkgfile, installfile)
+        
+    #print(basefile)
