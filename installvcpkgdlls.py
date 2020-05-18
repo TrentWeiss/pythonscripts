732ef740206568f32457b3adeb2d27458ca5fdd5
Trent Weiss <ttw2xk@virginia.edu>
2020-05-18 13:52:58 -0400

    default vcpkg bin dir to an environment variable

--- installvcpkgdlls.py
+++ installvcpkgdlls.py
@@ -3,11 +3,13 @@ from shutil import copyfile
 parser = argparse.ArgumentParser("Automatically copy dll dependencies from vcpkg")
 parser.add_argument("buildpath", type=str)
 parser.add_argument("installbin", type=str)
-parser.add_argument("vcpkgbin", type=str)
+parser.add_argument("--vcpkgbin", type=str, default=os.getenv("VCPKG_BIN_DIR"), required=False)
 args = parser.parse_args()
 buildpath = args.buildpath
 installbin = args.installbin
 vcpkgbin = args.vcpkgbin
+if vcpkgbin is None:
+    raise ValueError("you must either specify a vcpkg binary directory with --vcpkgbin or set the VCPKG_BIN_DIR environment variable")
 files = glob.glob(os.path.join(buildpath,"**","*.dll"),recursive=True)
 for f in files:
     basefile = os.path.basename(f)
