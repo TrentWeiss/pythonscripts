fdb27e76b9856e345338a6fee658542199d1dc0b
Trent Weiss <ttw2xk@virginia.edu>
2020-05-19 08:59:38 -0400

    sure

--- backup_folders.py
+++ backup_folders.py
@@ -12,11 +12,11 @@ def sevenZipFolder(folder: str):
     output = subprocess.run(["7z", "a", archive_name, folder])
     return archive_name
 
-parser = argparse.ArgumentParser("Back up folders and scp it to the CS department's systems")
+parser = argparse.ArgumentParser("7z folders and scp the resulting archives to a backup server")
 parser.add_argument('folders', metavar='folder', type=str, nargs='+',  help='The folders to back up')
-parser.add_argument('--username', type=str, default="ttw2xk",  help="Username on the CS systems")
-parser.add_argument('--host', type=str, default="portal.cs.virginia.edu" , help="Host to scp to")
-parser.add_argument('--output_dir', type=str,default="/p/DeepRacing/datasets",  help='Directory to put the resulting archives on the CS backup')
+parser.add_argument('--host', type=str, required=True , help="Host to scp to")
+parser.add_argument('--username', type=str, required=True ,  help="Username on the backup system")
+parser.add_argument('--output_dir', type=str, default="~" ,  help='Directory to put the resulting archives on backup system')
 args = parser.parse_args()
 folders = args.folders
 output_dir = args.output_dir
@@ -24,14 +24,15 @@ username = args.username
 host = args.host
 print(folders)
 print(output_dir)
-password = getpass.getpass(prompt='Password: ', stream=None)
+password = getpass.getpass(prompt='Password for %s on %s: ', (username,host), stream=None)
 ssh = SSHClient()
 ssh.load_system_host_keys()
 ssh.connect(host,username=username,password=password)
 ssh.close()
 for folder in folders:
     archive_name = sevenZipFolder(folder)
-    print("uploading %s to %s" %(archive_name, host+":"+os.path.join(output_dir,archive_name)))
+    output_archive_name = os.path.join(output_dir,archive_name).replace("\\","/")
+    print("uploading %s to %s" %(archive_name, host+":"+output_archive_name))
     ssh = SSHClient()
     ssh.load_system_host_keys()
     ssh.connect(host,username=username,password=password)
