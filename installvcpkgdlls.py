29f5b1954857183b33fccedd75069e890916384a
Trent Weiss <ttw2xk@virginia.edu>
2020-05-20 19:13:01 -0400

    update docs

--- installvcpkgdlls.py
+++ installvcpkgdlls.py
@@ -5,7 +5,7 @@ from shutil import copyfile
 parser = argparse.ArgumentParser("Automatically copy dll dependencies from vcpkg. Recursively searches a build tree for dll files that vcpkg copied in as part of the build.  This script then copies that file to a specified install directory IF AND ONLY IF that same dll file is also found in a specified vcpkg bin folder.")
 parser.add_argument("buildpath", type=str, help="Recursively search this folder for .dll files")
 parser.add_argument("installbin", type=str, help="Where to put the found dlls if they are part of a vcpkg installation")
-parser.add_argument("--vcpkgbin", type=str, default=os.getenv("VCPKG_BIN_DIR"), required=False, help="bin directory of vcpkg installation")
+parser.add_argument("--vcpkgbin", type=str, default=os.getenv("VCPKG_BIN_DIR"), required=False, help="bin directory of vcpkg installation. Defaults to an environment variable named VCPKG_BIN_DIR")
 parser.add_argument("--force", action="store_true", help="Overwrite dlls that already exist in the installbin folder")
 args = parser.parse_args()
 buildpath = args.buildpath
