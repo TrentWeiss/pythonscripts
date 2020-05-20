4a1c2e46984677225119c36686046b1cd1411678
Trent Weiss <ttw2xk@virginia.edu>
2020-05-20 19:02:42 -0400

    allow option to overwrite existing dlls

--- installvcpkgdlls.py
+++ installvcpkgdlls.py
@@ -6,10 +6,12 @@ parser = argparse.ArgumentParser("Automatically copy dll dependencies from vcpkg
 parser.add_argument("buildpath", type=str)
 parser.add_argument("installbin", type=str)
 parser.add_argument("--vcpkgbin", type=str, default=os.getenv("VCPKG_BIN_DIR"), required=False)
+parser.add_argument("--vcpkgbin", type=bool, default=False, required=False, help="Overwrite dlls that already exist in the installbin folder")
 args = parser.parse_args()
 buildpath = args.buildpath
 installbin = args.installbin
 vcpkgbin = args.vcpkgbin
+force=args.force
 if vcpkgbin is None:
     raise ValueError("you must either specify a vcpkg binary directory with --vcpkgbin or set the VCPKG_BIN_DIR environment variable")
 files = glob.glob(os.path.join(buildpath,"**","*.dll"),recursive=True)
@@ -17,7 +19,7 @@ for f in files:
     basefile = os.path.basename(f)
     installfile = os.path.join(installbin,basefile)
     vcpkgfile = os.path.join(vcpkgbin,basefile)
-    if (not os.path.exists(installfile)) and os.path.exists(vcpkgfile):
+    if os.path.exists(vcpkgfile) and ((not os.path.exists(installfile)) or force) :
         print("Installing " + vcpkgfile + " to " + installfile)
         copyfile(vcpkgfile, installfile)
         
