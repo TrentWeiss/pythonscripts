fb08945c0511bd40e48e7c23a49f57f6d29da590
Trent Weiss <ttw2xk@virginia.edu>
2021-09-05 17:08:56 -0400

    yay

--- unpack7zips.py
+++ unpack7zips.py
@@ -1,9 +1,16 @@
 import glob
 import py7zr
 import os
+import argparse
 
-scriptdir=os.path.abspath(os.path.dirname(__file__))
-archives=glob.glob(os.path.join(scriptdir,"**","*.7z"), recursive=True)
+parser = argparse.ArgumentParser(description="Unpack all .7z files in a specified directory")
+parser.add_argument("root_dir", help="Where to search for .7z files")
+args = parser.parse_args()
+
+
+
+root_dir=os.path.abspath(args.root_dir)
+archives=glob.glob(os.path.join(root_dir,"**","*.7z"), recursive=True)
 print(archives)
 for archivepath in archives:
   archivedir = os.path.dirname(archivepath)
